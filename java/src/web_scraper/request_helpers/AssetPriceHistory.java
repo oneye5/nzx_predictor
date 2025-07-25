6707f4133ffd522c67b01e37053a1ff22f45ffd2
Owan Lazic <ocjlaz@gmail.com>
2025-07-25 21:54:44 +1200

    Start implementation of price parsing

--- java/src/web_scraper/request_helpers/AssetPriceHistory.java
+++ java/src/web_scraper/request_helpers/AssetPriceHistory.java
@@ -1,12 +1,39 @@
 package web_scraper.request_helpers;
 
+import com.google.gson.Gson;
 import misc.Pair;
+import pojos.yahoo.financials.FinancialInformation;
+import pojos.yahoo.prices.HistoricPriceInformation;
 import pojos.yahoo.prices.Meta;
+import web_scraper.ApiUrls;
 
-import java.util.Map;
+import java.util.*;
 
 public record AssetPriceHistory (Map<String, Map<Long, Pair<DataPoint, Meta>>> data) {
+	public static AssetPriceHistory get(List<String> tickers) {
+		Gson gson = new Gson();
+		Map<String, Map<Long,Pair<DataPoint,Meta>>> out = new HashMap<>();
+		tickers.stream().parallel().forEach(ticker -> {
+			String url = ApiUrls.getHistoricPricesUrl(ticker);
+			HistoricPriceInformation data = null;
 
+			try { data = gson.fromJson(url, HistoricPriceInformation.class); }
+			catch (Exception ignored) {}
+
+			Map<Long, Pair<DataPoint, Meta>> unpacked = unpack(data);
+
+			if(unpacked != null)
+				out.put(ticker, unpacked);
+		});
+
+		return null;
+	}
+
+	private static Map<Long, Pair<DataPoint, Meta>> unpack(HistoricPriceInformation data) {
+		if(data == null) return null;
+
+
+	}
 }
 
 record DataPoint (
